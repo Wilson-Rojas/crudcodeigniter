eacb0c0a402ef785ac9ec8b5b3994ba4d41b3812
Wilson-Rojas <117527439+Wilson-Rojas@users.noreply.github.com>
2024-04-02 07:55:03 -0400

    subida de ajax

--- app/Views/form.php
+++ app/Views/form.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!-- STYLES -->
 
     <style {csp-style-nonce}>
@@ -239,7 +239,7 @@
                     <label class="form-label" >Correo</label>
                     <input type="email" class="form-control" name="correo" placeholder="example@email.com" id=""> 
                     <br>
-                    <button class="btn btn-primary mb-3" type="submit" >Enviar</button>
+                    <button class="btn btn-primary mb-3" type="submit" >Guardar</button>
                 </div>
             </div>
         </form>
@@ -267,10 +267,31 @@
                         <a href="<?php echo base_url().'/obtenerNombre/'.$key->form_id ?>" class="btn btn-warning btn-sm">Editar</a>
                     </td>
                     <td>
-                        <button class="btn btn-danger btn-sm btnEliminar" data-id="<?php echo $key->form_id ?>">Eliminar</button>
+                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal-<?php echo $key->form_id ?>">
+                            Eliminar
+                        </button>
                     </td>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal-<?php echo $key->form_id ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Eliminar el registro </h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>¿Seguro que desea eliminar el registro de : <?php echo $key->nombrecompleto ?>?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-success" data-bs-dismiss="modal">Cerrar</button>
+                            <button type="button" class="btn btn-danger  btnEliminar" data-id="<?php echo $key->form_id ?>" data-bs-dismiss="modal">Eliminar</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                 </tr>
             <?php endforeach; ?>
+            
         </table>
     </div>
 </section>
